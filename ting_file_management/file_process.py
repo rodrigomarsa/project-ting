87e433c484ad81c827a9768f0d9bd64d699b6429
Rodrigo <rodrigomartins.agro@gmail.com>
2023-06-13 17:34:30 -0300

    feat: add file_process process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(0, instance.__len__()):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    dicio = dict(
+        nome_do_arquivo=path_file,
+        qtd_linhas=len(txt_importer(path_file)),
+        linhas_do_arquivo=txt_importer(path_file),
+    )
+    instance.enqueue(dicio)
+    print(instance.search(0), file=sys.stdout)
 
 
 def remove(instance):
