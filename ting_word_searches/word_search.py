5552c7532f776037e61b2fc7ac34a176d4c1f8b3
Rodrigo <rodrigomartins.agro@gmail.com>
2023-06-14 15:43:06 -0300

    feat: add word_search exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocorrencias = []
+    for index in range(0, instance.__len__()):
+        nome_do_arquivo = instance.search(index)["nome_do_arquivo"]
+        linhas_do_arquivo = instance.search(index)["linhas_do_arquivo"]
+        for index, linha in enumerate(linhas_do_arquivo):
+            if word.lower() in linha.lower():
+                ocorrencias.append({"linha": index + 1})
+    dicio = [
+        dict(
+            palavra=word,
+            arquivo=nome_do_arquivo,
+            ocorrencias=ocorrencias,
+        )
+    ]
+    return dicio if ocorrencias else []
 
 
 def search_by_word(word, instance):
